97c3148b28158954458c1c00614f5ee69e451798
bgriffff <brandonjosephgriffiths@gmail.com>
2023-02-03 16:30:27 -0700

    Fixed

---
+++ Mission_4_Brandon_Griffiths/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission_4_Brandon_Griffiths.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+    }
+}
